6df4d00b75ad3fffada5398ae72e9e8e1511e78a
Andrii Hreshchik <ahresh@softserveinc.com>
2019-05-08 20:15:36 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react"
+import Conditional from "./Conditional"
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            isLoading: true
+        }
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                isLoading: false
+            })
+        }, 1500)
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.isLoading ?
+                    <h1>Loading...</h1> :
+                    <Conditional />}
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
